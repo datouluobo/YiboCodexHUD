519e70d678f28a3185673fa97bc6d8e7bb8b2b33
datouluobo <datouluobo@gmail.com>
2026-07-10 22:26:47 +0800

    align installer release script version

--- installer/build-installer.ps1
+++ installer/build-installer.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Version = "1.0.2",
+    [string]$Version = "1.0.3",
     [string]$PublishDir = ".tmp\release\v$Version\win-x64",
     [string]$OutputDir = ".tmp\installer\v$Version"
 )
